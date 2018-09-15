c4bff93a2f8df296e72c8d289b070506311ee16a
AmicableBruda <43212443+AmicableBruda@users.noreply.github.com>
2018-09-14 23:18:44 -0400

    Update Listener Tools Update.user.js

--- Listener Tools Update.user.js
+++ Listener Tools Update.user.js
@@ -1,8 +1,6 @@
 // ==UserScript==
 // @name         Listener Tools
 // @license      MPL-2.0
-// @namespace    https://github.com/AmicableBruda/7cups
-// @supportURL   https://github.com/AmicableBruda/7cups/issues
-// @version      0.2
+// @version      0.3
 // @author       AmicableBruda
 // ==/UserScript==
